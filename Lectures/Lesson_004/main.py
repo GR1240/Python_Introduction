57c18f90055a610ff06fdb54a0733750eff008ba
GR1240 <ruslan1240@mail.ru>
2024-02-25 23:17:01 +0300

    add Lesson 004 - Higher-order functions, working with files

---
+++ Lectures/Lesson_004/main.py
@@ -0,0 +1,134 @@
+
+
+
+
+
+# Задача
+# В списке хранятся числа. Нужно выбрать только чётные числа и составить
+# список пар (число; квадрат числа).
+# Пример: 1 2 3 5 8 15 23 38
+# Получить: [(2, 4), (8, 64), (38, 1444)]
+
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# out = []
+# for i in data :
+#     if i % 2 == 0:
+#         out.append((i, i ** 2))
+# print(out)
+
+# # ----------------------
+
+# def select(f, col):
+#     return [f(x) for x in col]
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = select(int, data)
+# print(res)
+# res = where(lambda x: x % 2 == 0, res)
+# print(res)
+# res = list(select(lambda x: (x, x**2), res))
+# print(res)
+
+
+# ----------------------------------------------
+
+# Функция map
+
+# list_1 = [x for x in range(1, 20)]
+# print(list_1)
+
+# list_1 = list(map(lambda x: x + 10, list_1))
+# print(list_1)
+
+# Задача. 
+# C клавиатуры вводится некий набор чисел, в качестве разделителя используется пробел. 
+# Этот набор чисел будет считан в качестве строки. 
+# Как превратить list строк в list чисел?
+
+# data = '1 2 3 5 8 15 23 38'
+# print(data) 
+
+# data = list(map(int, data.split()))
+# print(data)
+
+# # -------------------
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = map(int, data)
+# print(res)
+# res = where(lambda x: x % 2 == 0, res)
+# print(res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+# # -------------------
+
+# Функция filter
+
+# data = [15, 65, 9, 36, 175]
+# res = list(filter(lambda x: x % 10 == 5, data))
+# print(res)
+
+# # -------------------
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = map(int, data)
+# res = filter(lambda x: x % 2 == 0, res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+# # --------------------------
+
+# Функция zip
+
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# data = list(zip(users, ids))
+# print(data) # [('user1', 4), ('user2', 5), ('user3', 9), ('user4', 14), ('user5', 7)]
+
+
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
+# data = list(zip(users, ids, salary))
+# print(data) # [('user1', 4, 111), ('user2', 5, 222), ('user3', 333)]
+
+
+# Функция enumerate
+# users = ['user1', 'user2', 'user3']
+# data = list(enumerate(users))
+# print(data) # [(0, 'user1'), (1, 'user2'), (2, 'user3))]
+
+# # --------------------------
+
+# Файлы
+
+# Режим a
+colors = ['red', 'green', 'blue']
+data = open('file.txt', 'a') # здесь указываем режим, в котором будем работать
+data.writelines(colors) # разделителей не будет
+data.close()
+
+with open('file.txt', 'w') as data:
+    data.write('line 1\n')
+    data.write('line 2\n')
+
+# Режим r
+path = 'file.txt'
+data = open(path, 'r')
+for line in data:
+    print(line)
+data.close()
+
+# Режим w
+colors = ['red', 'green', 'blue']
+data = open('file.txt', 'w')
+data.writelines(colors) # разделителей не будет
+data.close()
